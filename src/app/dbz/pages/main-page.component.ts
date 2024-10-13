1787e80127c6322951a2f0639e75d0a63af7e349
Gabriel <c.gabrielkarajallo@gmail.com>
2024-10-13 19:45:22 +0200

    angular

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -8,14 +9,8 @@ import { Character } from '../interfaces/character.interface';
 
 export class MainPageComponent {
 
+  constructor( public dbzService: DbzService ){
 
-  public characters: Character[] = [
-    {name:'Krillin', power: 1000},
-    {name: 'Goku', power: 9500},
-    {name: 'Vegeta', power: 7500 }
-  ];
-
-  public onNewCharacter( character: Character ):void{
-    this.characters.push(character);
   }
+
 }
